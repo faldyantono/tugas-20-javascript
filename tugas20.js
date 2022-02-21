11513ee9fb00dc300d3d6ca450d6b2cbc3b5bb47
faldyantono <96801457+faldyantono@users.noreply.github.com>
2022-02-21 07:31:35 +0700

    Add files via upload

---
+++ tugas20.js
@@ -0,0 +1,6 @@
+function main() {
+  var sentences = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.";
+  console.log(sentences.match("are"));
+  console.log(sentences.match("words"));
+}
+main()
